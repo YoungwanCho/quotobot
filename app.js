d3ff81ef9cefe0ccdf5f9175de976f43e5003f7d
developer119 <developer119korea@gmail.com>
2018-05-12 19:41:23 +0900

    슬랙 봇에게 오늘의 명언이라고 메세지를 보내면 답을 해준다.

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 var Bot = require('slackbots');
+const fs = require('fs');
 
 var setting = {
-    token:'',
-    name:'quotebot'
+    token: '토큰을 입력하세요',
+    name: 'quotebot'
 };
 
 var bot = new Bot(setting);
 
-bot.on('start', function() {
-// bot.postMessageToChannel('general', 'Hi Channel.');
-bot.postMessageToUser('developer119', 'Hi User.');
-// bot.postMessageToGroup('tisdoksend', 'Hi private grop.');
+isChatMsg = function (msg) {
+    return msg.type === 'message';
+};
+
+isFromQuoteBot = function (msg) {
+    return msg.username === 'quotebot';
+};
+
+isMentioningQuote = function (msg) {
+    return msg.text.toLowerCase().indexOf('오늘의 명언') > -1;
+};
+
+replyWithRandomQuote = function (bot, oMsg) {
+    fs.readFile(__dirname + "/data/" + "wisesaying.json", 'utf8', function (err, data) {
+        var wisesayingtable = JSON.parse(data);
+        var count = Object.keys(wisesayingtable).length;
+        var randomIndex = Math.floor(Math.random() * count);
+        var message = wisesayingtable[randomIndex].message + '\n'
+            + '- ' + wisesayingtable[randomIndex].name + " -";
+        bot.postMessageToUser('developer119', message);
+    });
+};
+
+bot.on('message', function (msg) {
+    console.log(msg);
+    if (isChatMsg(msg) &&
+        !isFromQuoteBot(msg) &&
+        isMentioningQuote(msg)) {
+        replyWithRandomQuote(bot, msg);
+    }
 });
